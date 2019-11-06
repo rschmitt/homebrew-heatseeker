9ad07a8a522b946de8c134a7dfef8069dba4b59e
Ryan Schmitt <rschmitt@pobox.com>
2019-11-05 23:39:12 -0800

    Upgrade to v1.7.0

--- Formula/heatseeker.rb
+++ Formula/heatseeker.rb
@@ -1,8 +1,8 @@
 class Heatseeker < Formula
   desc 'A high-performance general purpose fuzzy finder, based on selecta'
   homepage 'https://github.com/rschmitt/heatseeker'
-  url 'https://github.com/rschmitt/heatseeker/releases/download/v1.6.1/heatseeker-v1.6.1-x86_64-apple-darwin.tar.gz'
-  sha256 '4aa041e6b99467b0735097f010bf69ac38dee686aa4e24cc80ddd653582068f3'
+  url 'https://github.com/rschmitt/heatseeker/releases/download/v1.7.0/heatseeker-v1.7.0-x86_64-apple-darwin.tar.gz'
+  sha256 '8991c054bf5bd5000eedd5305a8e8426eb098778715debd68e926fce191fe54a'
 
   def install
     bin.install 'hs'
